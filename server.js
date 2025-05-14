a7b03722ec899a550fd90bccc97fa9d67d1dd3b7
Arithmathsolver <daniellemayian22@gmail.com>
2025-05-14 20:05:04 +0300

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Proper use of body-parser for JSON payloads
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
